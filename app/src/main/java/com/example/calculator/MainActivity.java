b151b77275755de963500a889c28ff0037fdd0cf
KL School <klatupalli@gmail.com>
2021-10-18 11:56:26 -0400

    Fix layout

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
+    Button b5;
+    Button b6;
+    Button b7;
+    Button b8;
+    Button b9;
+    Button b0;
+    Button bP;
+    Button bM;
+    Button bD;
+    Button bE;
+    Button bC;
+    TextView ans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b0 = findViewById(R.id.button0);
+
     }
 }
