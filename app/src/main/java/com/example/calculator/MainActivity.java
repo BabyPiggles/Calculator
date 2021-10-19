4b676061689666e9b5d3fb004b43883101e9aa82
Kushaal Latupalli <klatupalli@gmail.com>
2021-10-19 00:02:48 -0400

    Finish work method and finished turning string to tokens to arraylist

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.ArrayLinkedVariables;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         bP = findViewById(R.id.buttonPlus);
         bS = findViewById(R.id.buttonMinius);
         bM = findViewById(R.id.buttonMulti);
-        bD = findViewById(R.id.buttonMulti);
+        bD = findViewById(R.id.buttonDiv);
         bE = findViewById(R.id.buttonEquals);
         bC = findViewById(R.id.buttonClear);
         dP = (TextView)findViewById(R.id.textViewDP);
@@ -58,41 +61,83 @@ public class MainActivity extends AppCompatActivity {
         switch(v.getId())
         {
             case R.id.button0:
-                work("0");
+                work(" 0");
                 break;
             case R.id.button1:
-                work("1");
+                work(" 1");
                 break;
             case R.id.button2:
-                work("2");
+                work(" 2");
                 break;
             case R.id.button3:
-                work("3");
+                work(" 3");
                 break;
             case R.id.button4:
-                work("4");
+                work(" 4");
                 break;
             case R.id.button5:
-                work("5");
+                work(" 5");
                 break;
             case R.id.button6:
-                work("6");
+                work(" 6");
                 break;
             case R.id.button7:
-                work("7");
+                work(" 7");
                 break;
             case R.id.button8:
-                work("8");
+                work(" 8");
                 break;
-
+            case R.id.button9:
+                work(" 9");
+                break;
+            case R.id.buttonPlus:
+                work(" +");
+                break;
+            case R.id.buttonMinius:
+                work(" -");
+                break;
+            case R.id.buttonMulti:
+                work(" *");
+                break;
+            case R.id.buttonDiv:
+                work(" /");
+                break;
+            case R.id.buttonEquals:
+                calculations();
+                break;
+            case R.id.buttonClear:
+                dP.setText("0");
+                break;
 
         }
     }
     public void work(String s)
     {
+        if(dP.getText().toString().equals("0"))
+        {
+            ans = dP.getText().toString();
+            ans = s;
+            dP.setText(ans);
+        }
+        else
+        {
+            ans = dP.getText().toString();
+            ans += s;
+            dP.setText(ans);
+        }
+
+    }
+    public void calculations()
+    {
         ans = dP.getText().toString();
-        ans += s;
-        dP.setText(ans);
+        StringTokenizer st = new StringTokenizer(ans," ");
+        ArrayList<String> calc = new ArrayList<String>();
+        do
+        {
+            calc.add(st.nextToken());
+        }while(st.hasMoreTokens()==true);
+        
+
     }
 }
 
